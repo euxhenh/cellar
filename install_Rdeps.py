6ed21de273974e2ea8b5b3274c329a535b08de7b
ferrocactus <ehasanaj@cs.cmu.edu>
2021-11-08 15:23:48 -0500

    Fixed R library installation

--- install_Rdeps.py
+++ install_Rdeps.py
@@ -5,7 +5,7 @@ import rpy2.robjects.packages as rpackages
 utils = rpackages.importr('utils')
 utils.chooseBioCmirror(ind=1)
 utils.chooseCRANmirror(ind=1)  # select the first mirror in the list
-packnames = ('devtools', 'BiocManager', 'BiocParallel', 'flowCore')
+packnames = ('devtools', 'BiocManager')
 names_to_install = [x for x in packnames if not rpackages.isinstalled(x)]
 
 print("Installing", names_to_install)
@@ -14,6 +14,8 @@ if len(names_to_install) > 0:
     utils.install_packages(StrVector(names_to_install))
 
 BiocManager = importr('BiocManager')
+BiocManager.install('BiocParallel')
+BiocManager.install('flowCore')
 BiocManager.install('SingleR')
 BiocManager.install('celldex')
 
