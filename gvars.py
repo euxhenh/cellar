102cf7ad796c815af3c3ec6a6e808539a42d5c65
ferrocactus <ehasanaj@cs.cmu.edu>
2021-11-13 18:06:17 -0500

    Added stvea dataset names

--- gvars.py
+++ gvars.py
@@ -3,7 +3,7 @@ DATA_PATH = 'data'
 CELLAR_LOGO = 'assets/cellar-logo-white.png'
 
 DOCS_URL = 'https://ferrocactus.github.io/CellarV/'
-GITHUB_URL = 'https://github.com/ferrocactus/cellarV'
+GITHUB_URL = 'https://github.com/ferrocactus/CellarV'
 DEMOS_URL = 'https://www.youtube.com/playlist?list=PL5sLSLkTYpWgfBQ0M8ObfBIqDMAzx0-D2'
 PAPER_URL = 'https://www.biorxiv.org/content/10.1101/2021.03.19.436162v1?rss=1'
 EMAIL ='mailto:ehasanaj@cs.cmu.edu?subject=Cellar'
